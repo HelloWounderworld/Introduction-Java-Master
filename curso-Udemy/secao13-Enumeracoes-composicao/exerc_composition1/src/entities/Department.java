b85c356b080f9dee85b57340f222ce672b59d6be
Leonardo Takashi Teramatsu <leonardo.teramatsu@gmail.com>
2023-03-07 15:52:51 -0300

    Atualizando as minhas notas

---
+++ curso-Udemy/secao13-Enumeracoes-composicao/exerc_composition1/src/entities/Department.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Department {
+
+	private String name;
+	
+	public Department() {
+	}
+
+	public Department(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+	
+}
---
+++ curso-Udemy/secao13-Enumeracoes-composicao/exerc_composition1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ltakashi
+ *
+ */
+module exerc_composition1 {
+}
