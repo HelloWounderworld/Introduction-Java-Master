e60bc74ca70352a4d435091876f2a2663878c1a6
Leonardo Takashi Teramatsu <leonardo.teramatsu@gmail.com>
2023-03-10 12:55:25 -0300

    Atualizando a minha nota

--- curso-Udemy/secao15-Tratamento-de-excecoes/exception_good_solution/src/model/entities/Reservation.java
+++ curso-Udemy/secao15-Tratamento-de-excecoes/exception_good_solution/src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	
 	private Integer roomNumber;
@@ -13,6 +15,9 @@ public class Reservation {
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
 	
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if(!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -40,6 +45,12 @@ public class Reservation {
 	}
 	
 	public void updateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
