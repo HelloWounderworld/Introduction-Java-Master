53b5cbaf6b0dfb9352fbe5011c8c994ac95a3634
Leonardo Takashi Teramatsu <vercel@buudigital.com>
2024-02-03 17:46:03 -0300

    Atualizando a minha nota

--- curso-Udemy/secao22-Acesso-a-banco-de-dados-com-JDBC/exercicios/src/jdbc/DAOTeste.java
+++ curso-Udemy/secao22-Acesso-a-banco-de-dados-com-JDBC/exercicios/src/jdbc/DAOTeste.java
@@ -6,10 +6,15 @@ public class DAOTeste {
 		
 		DAO dao = new DAO();
 		
-		String sql = "INSERT INTO pessoas (nome) VALUES (?)";
-		System.out.println(dao.incluir(sql, "Albert Einstein")); // Vamos conseguir verificar os ids que foram gerados
-		System.out.println(dao.incluir(sql, "Alan Turing"));
-		System.out.println(dao.incluir(sql, "Kiyoshi Oka"));
+//		String sql = "INSERT INTO pessoas (nome) VALUES (?)";
+		String sql = "INSERT INTO pessoas (nome, codigo) VALUES (?, ?)";
+//		System.out.println(dao.incluir(sql, "Albert Einstein")); // Vamos conseguir verificar os ids que foram gerados
+//		System.out.println(dao.incluir(sql, "Alan Turing"));
+//		System.out.println(dao.incluir(sql, "Kiyoshi Oka"));
+		
+		System.out.println(dao.incluir(sql, "Albert Einstein", 1000)); // Vamos conseguir verificar os ids que foram gerados
+		System.out.println(dao.incluir(sql, "Alan Turing", 1001));
+		System.out.println(dao.incluir(sql, "Kiyoshi Oka", 1002));
 		
 		dao.close();
 	}
