ab4d3432da41b1fc9d54b7aa7dff6de4bd56accb
Leonardo Takashi Teramatsu <vercel@buudigital.com>
2024-02-03 23:28:39 -0300

    Atualizando a minha nota

--- curso-Udemy/secao24-Java-EE-Mapeamento-objeto-relacional-com-JPA-Hibernate/exercicios-jpa/src/main/java/modelo/basico/Usuario.java
+++ curso-Udemy/secao24-Java-EE-Mapeamento-objeto-relacional-com-JPA-Hibernate/exercicios-jpa/src/main/java/modelo/basico/Usuario.java
@@ -2,13 +2,14 @@ package modelo.basico;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Usuario {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
