3ec6ecede107366eac2a875e395c7b67457246f4
Leonardo Takashi Teramatsu <leonardo.teramatsu@gmail.com>
2024-05-21 13:10:39 +0900

    Update My Code

---
+++ my-projects/Clean-Code-Practice-Guide/chap05-list-5-5-WikiPage-Responder.java
@@ -0,0 +1,10 @@
+// Como organiza-los as funcoes dependentes para o programa
+// E exatamente a ordem de composicoes de funcoes que se aprende na matematica
+// f(g(x))=(f・g)(x)
+// X -> Y -> Z
+//   g    f -> f(g(x))=(f・g)(x)
+
+public class WikiPageResponder implements SecureResponder {
+    protected WikiPage page;
+    protected PageData pageData;
+}
