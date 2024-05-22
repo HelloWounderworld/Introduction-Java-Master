06c2c374c489cb65407842ad06839dd49a1ebfe8
Leonardo Takashi Teramatsu <leonardo.teramatsu@gmail.com>
2024-05-22 11:54:58 +0900

    Update my note

--- my-projects/Clean-Code-Practice-Guide/chap05-list-5-5-WikiPage-Responder.java
+++ my-projects/Clean-Code-Practice-Guide/chap05-list-5-5-WikiPage-Responder.java
@@ -4,6 +4,8 @@
 // X -> Y -> Z
 //   g    f -> f(g(x))=(f・g)(x)
 
+// Seguindo a Ordenacao Vertial o codigo abaixo esta satisfazendo essa boa pratica
+
 public class WikiPageResponder implements SecureResponder {
     protected WikiPage page;
     protected PageData pageData;
@@ -11,5 +13,42 @@ public class WikiPageResponder implements SecureResponder {
     protected Request request;
     protected PageCrawler crawler ;
 
-    
+    public Response makeResponse(FitNessContext context, Request request) throws Exception {
+        String pageName = getPageNameOrDefault(request, "FrontPage");
+        loadPage(pageName, context);
+        if (page == null)
+            return notFoundResponse(context, request);
+        else
+            return makePageResponse(context);
+    }
+
+    private String getPageNameOrDefault(Request request, String defaultPageName) {
+        String pageName = request.getResource();
+        if (StringUtil.isBlank(pageName))
+            pageName = defaultPageName;
+
+        return pageName;
+    }
+
+    protected void loadPage(String resource, FitNesseContext context) throws Exception {
+        WikiPagePath path = PathParser.parse(resource);
+        crawler = context.root.getPageCrawler();
+        crawler.setDeadEndStrategy(new VirtualEnabledPageCrawler());
+        page = crawler.getPage(context.root, path);
+        if (page != null) pageData = page.getData();
+    }
+
+    private Response notFoundResponse(FitNesseContext context, Request request) throws Exception {
+        return new NotFoundResponder().makeResponse(context, request);
+    }
+
+    private SimpleResponse makePageResponse(FitNesseContext context) throws Exception {
+        pageTitle = PathParser.render(crawler.getFullPath(page));
+        String html = makeHtml(context);
+
+        SimpleResponse response = new SimpleResponse();
+        response.setMaxAge(0);
+        response.SetContent(html);
+        return response;
+    }
 }
